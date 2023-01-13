b7882d8ba61d97ce815adb9ac2a560df25185476
Angela Londoño <l.angela05@gmail.com>
2023-01-13 11:02:41 -0500

    Create function Encriptar

---
+++ js/index.js
@@ -0,0 +1,7 @@
+const mensaje = document.getElementById('texto')
+const btnDesencriptar = document.getElementById('boton-desencriptar')
+const btnEncriptar = document.getElementById('boton-encriptar')
+
+function encriptar() {
+    
+}
